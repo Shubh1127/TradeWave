34222ed9cabcd420a31469ebbc42546a04f26723
shubham <shubhamsinghmor2312@gmail.com>
2024-10-06 21:51:40 +0530

    changes done

---
+++ dashboard/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import Home from "./components/Home";
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
